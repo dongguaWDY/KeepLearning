6525df999a439717becf367ade4b628e4f4487d1
WDY <2571790292@qq.com>
2016-12-15 03:00:32 +0800

    modify file

---
+++ DataStructureAndAlgorithms/OperationsOnTree.java
@@ -0,0 +1,21 @@
+
+//define Tree node
+class TreeNode{
+	public int value;
+	public TreeNode left;
+	public TreeNode right;
+}
+
+
+
+
+
+public class OperationsOnTree{
+	public static void main(String[] args){
+
+	}
+
+
+	//create a binary tree from a sequence of numbers
+	
+}
