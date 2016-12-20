33ed2979d6d8df913907342e5092b3d43437677e
WDY <2571790292@qq.com>
2016-12-21 03:35:09 +0800

    modify CollectionsSortedBy2Methods.java

--- Java基础知识/CollectionSortedBy2Methods.java
+++ Java基础知识/CollectionSortedBy2Methods.java
@@ -12,7 +12,9 @@ public class CollectionSortedBy2Methods{
 		Collections.sort(list);
 		System.out.println("after sorted:"+list);
 
+		//使用匿名内部类
 		Collections.sort(list,new Comparator<MyApple>(){
+
 			@Override
 			public int compare(MyApple o1, MyApple o2){
 				if(o1.volumn == o2.volumn){
@@ -24,13 +26,23 @@ public class CollectionSortedBy2Methods{
 				}
 			}
 
+			//Note that it is always safe not to override Object.equals(Object)(From JavaDoc)
 			/*
 			@Override
 			public boolean equals(Object obj){
 				return true;
 			}
 			*/
+
+			//匿名内部类不允许定义static方法，根本也没这个必要
+			/*
+			public static void callStaticMethod(){
+				System.out.println("in Static Method");
+			}
+			*/
+
 		});
+
 		System.out.println("after sorted again:"+list);
 	}
 }
@@ -58,5 +70,21 @@ class MyApple implements Comparable<MyApple>{
 		}else{
 			return -1;
 		}
+	}
+}
+
+
+
+class  test implements Comparator<test>{
+	/*
+	@Override
+	public int compare(test o1, test o2){
+		return 0;
+	}
+	*/
+
+	@Override
+	public boolean equals(Object obj){
+		return true;
 	}
 }
